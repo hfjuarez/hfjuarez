45bc01fd0936773b83f35d45faa263520cf75493
hfjuarez <54822117+hfjuarez@users.noreply.github.com>
2023-10-19 23:53:57 +0000

    Deploying to demo from @ hfjuarez/hfjuarez@cc9ef5559a09da21464c4b32facf6fae2fa3b992 🚀

--- _next/static/VHIorPhSbNoWYJYDi8Isd/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/341-41f06a90376a090f.js","static/css/885e5e3ac1b2a74d.css","static/chunks/pages/index-72a42c2109e77480.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
